91c8ad3cceab4ea2a3653f065383b21f18d4aaae
hwan <hwan.privac@gmail.com>
2020-02-29 22:22:44 +0900

    server, client running enabled

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,29 +1,41 @@
 const { join } = require('path');
 const webpack = require('webpack');
 const config = require('config');
+//cleans out bundle files whenever running webpack again.
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const GLOBALS = {
     'process.env': {
         NODE_ENV: JSON.stringify(config.get('NODE_ENV')),
         ENDPOINT: JSON.stringify(config.get('ENDPOINT')),
-        // RIA_SENTRY_APP: JSON.stringify(config.get('RIA_SENTRY_APP')),
-        // GOOGLE_API_KEY: JSON.stringify(config.get('GOOGLE_API_KEY')),
-        // FIREBASE_AUTH_DOMAIN: JSON.stringify(config.get('FIREBASE_AUTH_DOMAIN')),
-        // MAPBOX_API_TOKEN: JSON.stringify(config.get('MAPBOX_API_TOKEN'))
+        RIA_SENTRY_APP: JSON.stringify(config.get('RIA_SENTRY_APP')),
+        GOOGLE_API_KEY: JSON.stringify(config.get('GOOGLE_API_KEY')),
+        FIREBASE_AUTH_DOMAIN: JSON.stringify(config.get('FIREBASE_AUTH_DOMAIN')),
+        MAPBOX_API_TOKEN: JSON.stringify(config.get('MAPBOX_API_TOKEN'))
     }
 };
 
 module.exports = {
     devServer: {
-        // hot: true,
+        hot: true,
+        // content not from webpack path.
+        // contentBase: join(__dirname, "../src"),
+        open: true,
+        //This means that a script will be inserted in your bundle to take care of live reloading, and build messages will appear in the browser console.
         inline: true,
-        // historyApiFallback: true,
-    //     compress: true,
+        //serve index.html instead of any 404 response.
+        historyApiFallback: true,
+        //use gzip on contents
+        compress: true,
         port: 3000,
-        overlay: true
+        //Shows a full-screen overlay in the browser when there are compiler errors or warnings.
+        overlay: { 
+            warnings: true,
+            errors: true
+        }
     },
-    // devtool: 'source-map',
+    //when error occurs, it will point to exactly where it occured through mapping. 
+    devtool: 'source-map',
     entry: ['./src/index'],
     cache: true,
     output: {
@@ -32,50 +44,51 @@ module.exports = {
         filename: 'bundle.js'
     },
     plugins: [
-        // new webpack.NamedModulesPlugin(),
-        // new webpack.DefinePlugin(GLOBALS),
-        // new webpack.HotModuleReplacementPlugin(),
-        // new webpack.NoEmitOnErrorsPlugin(),
-        // new HtmlWebpackPlugin({
-        //     // Create HTML file that includes references to bundled CSS and JS.
-        //     template: 'src/index.ejs',
-        //     minify: {
-        //         removeComments: true,
-        //         collapseWhitespace: true,
-        //         removeRedundantAttributes: true,
-        //         useShortDoctype: true,
-        //         removeEmptyAttributes: true,
-        //         removeStyleLinkTypeAttributes: true,
-        //         keepClosingSlash: true,
-        //         minifyJS: true,
-        //         minifyCSS: true,
-        //         minifyURLs: true
-        //     },
-        //     inject: true
-        // })
+        //create global constants which can be configured at compile time.
+        new webpack.DefinePlugin(GLOBALS),
+        new webpack.HotModuleReplacementPlugin(),
+        //When the erorr occurs, do not refresh page
+        new webpack.NoEmitOnErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            // Create HTML file that includes references to bundled CSS and JS.
+            template: 'src/index.ejs',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            },
+            inject: true
+        })
     ],
     module: {
         rules: [
-            // {
-            //     test: /\.(eot|svg|woff|woff2|otf|ttf)$/,
-            //     exclude: /node_modules/,
-            //     loader: 'file-loader',
-            //     options: {
-            //         name: '[path][name].[ext]'
-            //     }
-            // },
+            {
+                test: /\.(eot|svg|woff|woff2|otf|ttf)$/,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            },
             {
                 test: /\.js$/,
                 include: join(__dirname, '../', 'src'),
                 loader: 'babel-loader'
             },
-            // { test: /\.(jpe?g|png|gif)$/i, loaders: ['file-loader'] },
-            // { test: /\.ico$/, loader: 'file-loader?name=[name].[ext]' },
-            // { test: /\.json$/, loader: 'json-loader' },
-            // {
-            //     test: /(\.css|\.scss)$/,
-            //     loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap']
-            // }
+            { test: /\.(jpe?g|png|gif)$/i, loaders: ['file-loader'] },
+            { test: /\.ico$/, loader: 'file-loader?name=[name].[ext]' },
+            { test: /\.json$/, loader: 'json-loader' },
+            {
+                test: /(\.css|\.scss)$/,
+                loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap']
+            }
         ]
     }
 };
--- server/server.js
+++ server/server.js
@@ -1,23 +1,17 @@
 // import { __PRODUCTION__ } from 'environs';
-let resolve =require('path');
-let bodyParser =require('body-parser');
-let express =require('express');
-let helmet =require('helmet');
-let cookieParser =require('cookie-parser');
-
-// import { resolve } from 'path';
-// import bodyParser from 'body-parser';
-// import compression from 'compression';
-// import cors from 'cors';
-// import express from 'express';
-// import helmet from 'helmet';
+import { resolve } from 'path';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import config from 'config';
+import hpp from 'hpp';
+import logger from 'morgan';
+import responseTime from 'response-time';
 // import favicon from 'serve-favicon';
-// import hpp from 'hpp';
-// import logger from 'morgan';
-// import cookieParser from 'cookie-parser';
-// import responseTime from 'response-time';
 // import * as firebase from 'firebase-admin';
-// import config from 'config';
 
 // Initialize Firebase
 // firebase.initializeApp({
@@ -26,36 +20,35 @@ let cookieParser =require('cookie-parser');
 // });
 
 // Our dummy database backend
-// import DB from '../db/DB';
-
+import DB from '../db/DB';
 // Modules explicitly related to React & SSR
-// import { renderToNodeStream } from 'react-dom/server';
-// import React from 'react';
-// import { match, RouterContext } from 'react-router';
-// import { Provider } from 'react-redux';
+import { renderToNodeStream } from 'react-dom/server';
+import React from 'react';
+import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // Modules from the client-side of things
-// import configureStore from '../src/store/configureStore';
-// import initialReduxState from '../src/constants/initialState';
-// import * as HTML from '../src/components/HTML';
-// import { routes } from '../src/routes';
-// import { loginSuccess } from '../src/actions/auth';
-// import { getPostsForPage } from '../src/actions/posts';
-// import { createError } from '../src/actions/error';
+import configureStore from '../src/store/configureStore';
+import initialReduxState from '../src/constants/initialState';
+import * as HTML from '../src/components/HTML';
+import { routes } from '../src/routes';
+import { loginSuccess } from '../src/actions/auth';
+import { getPostsForPage } from '../src/actions/posts';
+import { createError } from '../src/actions/error';
 
 // Create the express app and database
 const app = express();
-// const backend = DB();
+const backend = DB();
 
 // Add some boilerplate middlware
 // app.use(logger(__PRODUCTION__ ? 'combined' : 'dev'));
 app.use(helmet.xssFilter({ setOnOldIE: true }));
-// app.use(responseTime());
+app.use(responseTime());
 app.use(helmet.frameguard());
 app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
 app.use(helmet.hidePoweredBy({ setTo: 'react' }));
-// app.use(compression());
+app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 // app.use(hpp());
@@ -63,61 +56,63 @@ app.use(bodyParser.json());
 
 // other Route handlers
 // app.use('/api', backend);
-app.use('/static', express.static(resolve.resolve(__dirname, '..', 'static')));
+app.use('/static', express.static(resolve(__dirname, '..', 'static')));
 // app.use(favicon(resolve(__dirname, '..', 'static', 'assets', 'meta', 'favicon.ico')));
 app.use('*', (req, res) => {
     // Use React Router to match the incoming URL to a path
     // match({ routes: routes, location: req.originalUrl }, async (err, redirectLocation, props) => {
-    //     // Only redirect if necessary and if the user isn't on the login page (to prevent a loop)
-    //     if (redirectLocation && req.originalUrl !== '/login') {
-    //         return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-    //     }
-    //     // Create the store server-side using initial state constant
-    //     const store = configureStore(initialReduxState);
-    //     try {
-    //         // We've stored the user id in a cookie named letters-token,
-    //         // so we need to grab that here
-    //         const token = req.cookies['letters-token'];
-    //         if (token) {
-    //             // Get the firebase user from their token
-    //             // const firebaseUser = await firebase.auth().verifyIdToken(token);
-    //             // Normally we'd do something like query the database or send a request to
-    //             // another service/microservice, not the same server, but for our purposes
-    //             // this works
-    //             // const userResponse = await fetch(
-    //             //     `${config.get('ENDPOINT')}/users/${firebaseUser.uid}`
-    //             // );
-    //             // If a user can be found, load data for them
-    //             if (userResponse.status !== 404) {
-    //                 const user = await userResponse.json();
-    //                 // Redux-thunk allows us to wait for promises to finish, so we can dispatch
-    //                 // async  action creators and wait for them to finish before sending the
-    //                 // response back down to the browser
-    //                 await store.dispatch(loginSuccess(user));
-    //                 await store.dispatch(getPostsForPage());
-    //             }
-    //         }
-    //     } catch (err) {
-    //         // If the user's token is expired, wipe their token
-    //         if (err.errorInfo.code === 'auth/argument-error') {
-    //             res.clearCookie('letters-token');
-    //             return res.redirect(302, '/login');
-    //         }
-    //         // dispatch the error
-    //         store.dispatch(createError(err));
-    //     }
-    //     res.setHeader('Content-type', 'text/html');
-    //     res.write(HTML.start());
-    //     const renderStream = renderToNodeStream(
-    //         <Provider store={store}>
-    //             <RouterContext {...props} />
-    //         </Provider>
-    //     );
-    //     renderStream.pipe(res, { end: false });
-    //     renderStream.on('end', () => {
-    //         res.write(HTML.end(store.getState()));
-    //         res.end();
-    //     });
+        // Only redirect if necessary and if the user isn't on the login page (to prevent a loop)
+        // if (redirectLocation && req.originalUrl !== '/login') {
+        //     return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+        // }
+        // Create the store server-side using initial state constant
+        const store = configureStore(initialReduxState);
+        // try {
+            // We've stored the user id in a cookie named letters-token,
+            // so we need to grab that here
+            // const token = req.cookies['letters-token'];
+            // if (token) {
+                // Get the firebase user from their token
+                // const firebaseUser = await firebase.auth().verifyIdToken(token);
+                // Normally we'd do something like query the database or send a request to
+                // another service/microservice, not the same server, but for our purposes
+                // this works
+                // const userResponse = await fetch(
+                //     `${config.get('ENDPOINT')}/users/${firebaseUser.uid}`
+                // );
+                // If a user can be found, load data for them
+                // if (userResponse.status !== 404) {
+                    // const user = await userResponse.json();
+                    // Redux-thunk allows us to wait for promises to finish, so we can dispatch
+                    // async  action creators and wait for them to finish before sending the
+                    // response back down to the browser
+                    // await store.dispatch(loginSuccess(user));
+                    // await store.dispatch(getPostsForPage());
+                // }
+            // }
+        // } catch (err) {
+        //     // If the user's token is expired, wipe their token
+        //     if (err.errorInfo.code === 'auth/argument-error') {
+        //         res.clearCookie('letters-token');
+        //         return res.redirect(302, '/login');
+        //     }
+            // dispatch the error
+            // store.dispatch(createError(err));
+        // }
+        res.setHeader('Content-type', 'text/html');
+        res.write(HTML.start());
+        const renderStream = renderToNodeStream(
+            <Provider store={store}>
+                <StaticRouter location={req.url} context={{}}>
+                    {routes}
+                </StaticRouter>
+            </Provider>
+        );
+        renderStream.pipe(res, { end: false });
+        renderStream.on('end', () => {
+            res.write(HTML.end(store.getState()));
+            res.end();
+        });
     // });
 });
 
