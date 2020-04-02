7d18e2f89d54268c5daee812e030043b13300255
hwan <hwan.privac@gmail.com>
2020-04-02 13:44:22 +0900

    added static files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-// require('@std/esm');
-// require('babel-register');
+//Make ES2016 syntax available
+require('esm')(module)
+//All subsequent files required by node with the extensions .es6, .es, .jsx and .js will be transformed by Babel
+require('@babel/register');
+
 try {
     module.exports = require('./server/run');
 } catch (err) {
